c184a920b7f8b4e7e17109382fb26248cd52e782
maxim1436 <pivanov_m@mail.ru>
2022-03-05 18:09:19 +1000

    добавил функцию генерации данных

--- js/main.js
+++ js/main.js
@@ -57,6 +57,7 @@ const exceptions = {
   photos: [],
 };
 
+// eslint-disable-next-line no-shadow
 function getNonRepeatingNumber (array, elem, min, max) {
   if (array.includes(elem) === false) {
     array.push(elem);
@@ -70,30 +71,30 @@ function getNonRepeatingNumber (array, elem, min, max) {
 }
 
 const createElement = () => {
-  let avatarNumber = getNonRepeatingNumber(avatarExceptions, getRandomInt(0, 10), 0, 10);
+  const avatarNumber =  getNonRepeatingNumber(exceptions.avatar, getRandomInt(0, 10), 0, 10);
 
-  let featuresArray = [];
+  const featuresArray = [];
   for (let i = 0; i < getRandomInt(0, BUILDING_FEATURES.length -1); i++) {
-    let featureIndex = getNonRepeatingNumber(exceptions.features, getRandomInt(0, BUILDING_FEATURES.length -1), 0, BUILDING_FEATURES.length -1);
+    const featureIndex = getNonRepeatingNumber(exceptions.features, getRandomInt(0, BUILDING_FEATURES.length -1), 0, BUILDING_FEATURES.length -1);
     featuresArray.push(BUILDING_FEATURES[featureIndex]);
   }
   exceptions.features.length = 0;
 
-  let photosArray =[];
+  const photosArray =[];
   for(let i = 0; i < getRandomInt(0, BUILDING_PHOTOS.length -1); i++) {
-    let photoIndex = getNonRepeatingNumber(exceptions.photos, getRandomInt(0, BUILDING_PHOTOS.length -1), 0, BUILDING_PHOTOS.length -1);
+    const photoIndex = getNonRepeatingNumber(exceptions.photos, getRandomInt(0, BUILDING_PHOTOS.length -1), 0, BUILDING_PHOTOS.length -1);
     photosArray.push(BUILDING_PHOTOS[photoIndex]);
   }
   exceptions.photos.length = 0;
 
-  let lat = getRandomFraction(35.65000, 35.70000, 5);
-  let lng = getRandomFraction(139.70000, 139.80000, 5);
+  const lat = getRandomFraction(35.65000, 35.70000, 5);
+  const lng = getRandomFraction(139.70000, 139.80000, 5);
 
   return {
     author: {
-      avatar: ((avatarNumber == 0) || (avatarNumber ==10)) ?
-      `img/avatars/user${avatarNumber}.png` :
-      `img/avatars/user0${avatarNumber}.png`
+      avatar: ((avatarNumber === 0) || (avatarNumber === 10)) ?
+        `img/avatars/user${avatarNumber}.png` :
+        `img/avatars/user0${avatarNumber}.png`
     },
     offer: {
       title: 'Hello World!',
@@ -102,8 +103,8 @@ const createElement = () => {
       type: BUILDING_TYPE[getRandomInt(0, BUILDING_TYPE.length - 1)],
       rooms: getRandomInt(1, 10),
       guests: getRandomInt(1, 20),
-      checkin: CHECKIN_TIME[getRandomInt(0, CHECKIN_TIME.length - 1)],
-      checkout: CHECKIN_TIME[getRandomInt(0, CHECKIN_TIME.length - 1)],
+      checkin: CHECKIN_CHECKOUT_TIME[getRandomInt(0, CHECKIN_CHECKOUT_TIME.length - 1)],
+      checkout: CHECKIN_CHECKOUT_TIME[getRandomInt(0, CHECKIN_CHECKOUT_TIME.length - 1)],
       features: featuresArray,
       description: 'Comfortable apartments only for you!',
       photos: photosArray,
@@ -115,6 +116,5 @@ const createElement = () => {
   };
 };
 
+// eslint-disable-next-line no-unused-vars
 const array = Array.from({length: 10}, createElement);
-
-console.log(array);
