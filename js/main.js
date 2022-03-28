1aae05609be3d3c8df8e0ae3df946f979267ec1e
maxim1436 <pivanov_m@mail.ru>
2022-03-29 01:29:30 +1000

    Правда или действие(часть 1): исправления

--- js/main.js
+++ js/main.js
@@ -6,13 +6,4 @@ const SIMILAR_OFFER_COUNT = 10;
 const offersArray = Array.from({length: SIMILAR_OFFER_COUNT}, createElement);
 document.querySelector('#map-canvas').append(getNewCard(offersArray[0]));
 
-const form = document.querySelector('.ad-form');
-
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  const isValid = validateForm();
-  if (isValid) {
-    form.submit();
-  }
-});
-
+validateForm();
